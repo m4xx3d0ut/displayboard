959c50a723e62b215826b9d8ff9d31b3708eeaeb
Paul Kolesa <12650383+m4xx3d0ut@users.noreply.github.com>
2021-09-22 16:43:43 -0700

    Built structure of web app and started work on user management.

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/upload')
+@login_required
+def upload():
+    return render_template('upload.html', name=current_user.name)
+
+@main.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html')
