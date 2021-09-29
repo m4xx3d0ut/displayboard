05d5a90d4fc77990adb716ba75f329d141b2fc61
Paul Kolesa <12650383+m4xx3d0ut@users.noreply.github.com>
2021-09-29 16:15:05 -0700

    Wrote and tested logic for building playlists and managing content.  Working on templates for dynamic URLs.

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ main = Blueprint('main', __name__)
 # Allowed extension you can set your own
 ALLOWED_EXTENSIONS = set(['mp4', 'png', 'jpg', 'jpeg', 'gif'])
 
-# Content list TEST
-content = ['test.gif', 'test.mp4', 'test.png']
-
 # Playlist vars
 playfile = './playlist.txt'
 playlist = []
 playlen = []
-# include = []
+# Static files
+static = '%s/displayboard/static/' % (os.getcwd())
+
+# Content list TEST
+content = os.listdir(static)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @main.route('/')
 def index():
-    global playlist, playlen
+    global playlist, playlen, content
 
     # Check if playfile exists and init vars
     if os.path.isfile(playfile) and len(playlist) == 0:
@@ -33,13 +34,13 @@ def index():
                 playlen.append(line.split(',')[1].rstrip('\n'))
         pl.close
 
-    content = []
+    plcontent = []
 
     for i in range(0, len(playlist)):
-        content.append([playlist[i], playlen[i]])
+        if playlist[i] in content:
+            plcontent.append([playlist[i], playlen[i]])
 
-
-    return render_template('index.html', content=content)
+    return render_template('index.html', content=plcontent)
 
 @main.route('/upload', methods=['GET', 'POST'])
 @login_required
@@ -73,34 +74,67 @@ def admin():
 @main.route('/player', methods=['GET', 'POST'])
 @login_required
 def player():
-    global playlist, playlen
+    global playlist, playlen, content
 
     include = []
+    content = os.listdir(static)
+
+    def remzero():
+        global content
+        if len(delfiles) > 0 and delfiles[0] == 'on':
+            line_queue = []
+            print(content)
+            with open(playfile, 'r') as pl:
+                for line in pl.readlines():
+                    if line.split(',')[1].rstrip('\n') == '0':
+                        try:
+                            os.remove('%s%s' % (static, line.split(',')[0]))
+                        except:
+                            pass
+                    else:
+                        line_queue.append(line)
+            pl.close
+            content = os.listdir(static)
+            with open(playfile, 'w') as pl:
+                for line in line_queue:
+                    print(line)
+                    print(line.split(',')[0])
+                    print(line.split(',')[1])
+                    if line.split(',')[0] in content:
+                        print("write to pl")
+                        pl.write('%s%s' % (line.rstrip('\n'), '\n'))
+                    else:
+                        print("remove from pl")
+
+            pl.close
+            # content = os.listdir(static)
+            print(content)
+
+            flash('Deleting 0 duration files!')
+
 
     # Write data from select fields of /player to playfile
     if request.method == 'POST':
+        content = os.listdir(static)
         playlist = request.form.getlist('order')
         playlen = request.form.getlist('time')
-        include = request.form.getlist('include')
+        delfiles = request.form.getlist('remfiles')
         if os.path.isfile(playfile):
             os.remove(playfile)
         for i in range(0, len(playlist)):
             with open(playfile, 'a') as pl:
                 pl.write('%s,%s\n' % (playlist[i], playlen[i]))
             pl.close
-        # print(playlist)
-        # print(playlen)
-        print('Include:')
-        print(include)
         if request.form.get('remfiles') == 'on':
             print('Deleting unused files...')
         for i in range(0, len(playlist)):
-            print(playlist[i])
-            print(playlen[i])
             if int(playlen[i]) > 0:
+                remzero()
                 return redirect('/')
             else:
                 print('Failed')
+        remzero()
+        print(request.form.getlist('remfiles'))
         flash('Duration must exceed zero on selected content!')
         # return redirect('/')
 
