11019b8470d1fa3c733a21ecabbfcdf5ec551130
Paul Kolesa <12650383+m4xx3d0ut@users.noreply.github.com>
2021-10-05 10:44:18 -0700

    Added env_vars.sh test server startup script.

---
+++ env_vars.sh
@@ -0,0 +1,4 @@
+echo 'To start dev server: source ./displayboard/env_vars.sh'
+export FLASK_APP=/home/m4xx3d0ut/Documents/TS/git-projects/displayboard
+export FLASK_DEBUG=1
+flask run --host=0.0.0.0
